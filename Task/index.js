583e2432ef5efd9eff44723a38a6afcc084f8847
karankose <karankose13@gmail.com>
2025-02-01 15:49:41 +0530

    api

---
+++ Task/index.js
@@ -0,0 +1,86 @@
+const express = require ('express');
+const fs = require('fs');
+const app  = express();;
+const port = 8100;
+const users = require('./MOCK_DATA.json')
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true }))//use to get deta from form in body
+// middleware
+
+app.get('/api/users',(req , res,next)=>{
+   return res.json(users)
+})
+app.get('/users',(req , res , next)=>{
+    const html = `
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`)}
+    </ul>
+    `;
+    res.send(html)
+});
+
+ 
+
+
+ app.get('/users/:id', (req ,res, next)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    if(!user){
+      res.send(404+"  : user not found");
+    }
+    else{
+      const html = `<h1> User info </h1>
+      <h2> user id : ${user.id}</h2>
+      <h2> user name : ${user.first_name}</h2>
+      <h2> user email : ${user.email}</h2>`      
+      res.send(html);
+ }
+})
+app.post('/api/users',(req , res , next)=>{
+   // create user 
+   const body = req.body;
+   console.log(body);
+   users.push( {...body, id : users.length + 1 });
+   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err, data)=>{
+      return res.json(body);
+   }) 
+})
+
+
+
+
+app.route('/api/users/:id').get((req , res , next)=>{
+   const id = Number(req.params.id);// take query parameters in string Number to type change
+   const user = users.find((user)=> user.id === id)
+   if(!user){''
+      res.send(404+" user not found");
+   }
+   else{
+      return res.json(user);
+   }
+})
+.delete((req , res , next)=>{
+   console.log("delete")
+      const getId = parseInt(req.params.id)
+      console.log(getId);
+   
+   const user = users.find((user)=> user.id === getId)
+   if(!user){
+      res.send("User not found")
+   }{
+      users.splice( getId, 1 )
+      console.log( users.slice( getId , 1))
+      res.send("delete user ")
+   }
+   // res.json({task : " pending"})
+})
+.patch((req , res , next)=>{
+   // user edit
+   res.json({task : " pending"})
+})
+app.listen(port,()=>{
+    
+    console.log("http://localhost:8100");
+        
+})
